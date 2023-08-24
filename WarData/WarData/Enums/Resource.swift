34afdd6078dbd5482b5fb798460da6b4df18f397
yuraSabadin <yura.sabadin@icloud.com>
2023-08-24 16:30:56 +0300

    write fetch function add json files to Resource

---
+++ WarData/WarData/Enums/Resource.swift
@@ -0,0 +1,14 @@
+//
+//  Resource.swift
+//  WarData
+//
+//  Created by Yura Sabadin on 24.08.2023.
+//
+
+import Foundation
+
+enum Resource: String {
+    case main
+    case equipment
+    case personnel
+}
--- WarData/WarData/Network/NetworkManager.swift
+++ WarData/WarData/Network/NetworkManager.swift
@@ -9,24 +9,23 @@ import Foundation
 
 struct NetworkManager {
     
-    func fetchData(completionhandler: @escaping (Result<MainData?, Error>)->Void) {
+    func fetchData<T: Codable>(resource: Resource, of type: T.Type, completionhandler: @escaping (Result<T?, Error>)->Void) {
 
-        guard let url = Bundle.main.url(forResource: "main", withExtension: "json") else { print("не нашол файл")
+        guard let url = Bundle.main.url(forResource: resource.rawValue, withExtension: "json") else { print("file not find")
             return}
         do {
             let data = try Data(contentsOf: url)
-            let dataModel = self.parseJSON(data: data)
+            let dataModel = self.parseJSON(of: type, data: data)
             completionhandler(.success(dataModel))
         } catch {
             print("not catch")
         }
     }
     
-    private func parseJSON(data: Data) -> MainData? {
+    private func parseJSON<T: Codable>(of type: T.Type, data: Data) -> T? {
         let decoder = JSONDecoder()
         do{
-            let mainData = try decoder.decode(MainData.self, from: data)
-            print(mainData.count)
+            let mainData: T = try decoder.decode(T.self, from: data)
             return mainData
             
         } catch {
