581f87d5a2f20517fa4995e6302b0b432caa5a38
yuraSabadin <yura.sabadin@icloud.com>
2023-08-24 16:32:13 +0300

    some commit

--- WarData/WarData/Network/NetworkManager.swift
+++ WarData/WarData/Network/NetworkManager.swift
@@ -12,7 +12,7 @@ struct NetworkManager {
     func fetchData<T: Codable>(resource: Resource, of type: T.Type, completionhandler: @escaping (Result<T?, Error>)->Void) {
 
         guard let url = Bundle.main.url(forResource: resource.rawValue, withExtension: "json") else { print("file not find")
-            return}
+            return }
         do {
             let data = try Data(contentsOf: url)
             let dataModel = self.parseJSON(of: type, data: data)
